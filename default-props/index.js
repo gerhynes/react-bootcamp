0dc95d60c5cea73a0d698caa071296292ef421de
Gerard Hynes <gkhynes@gmail.com>
2019-06-02 20:17:18 +0100

    Add default-props example

---
+++ default-props/index.js
@@ -0,0 +1,12 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello to="Ringo" from="Paul" bangs={4} />
+        <Hello to="George" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
