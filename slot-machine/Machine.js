0ab415a74c80090664d97f15b26d24e1ae0484d4
Gerard Hynes <gkhynes@gmail.com>
2019-06-02 15:38:17 +0100

    Add slot-machine exercise

---
+++ slot-machine/Machine.js
@@ -0,0 +1,14 @@
+class Machine extends React.Component {
+  render() {
+    const { s1, s2, s3 } = this.props;
+    const winner = s1 === s2 && s2 === s3;
+    return (
+      <div>
+        <p>
+          {s1} {s2} {s3}
+        </p>
+        <p>{winner ? "You Win!" : "You lose!"}</p>
+      </div>
+    );
+  }
+}
