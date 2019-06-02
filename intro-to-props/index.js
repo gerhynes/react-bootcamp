4475f933089ed9a575ee414678e511255e9e5b4c
Gerard Hynes <gkhynes@gmail.com>
2019-06-02 15:13:54 +0100

    Update props exercise

--- intro-to-props/index.js
+++ intro-to-props/index.js
@@ -2,8 +2,8 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Hello to="Ringo" from="Paul" />
-        <Hello to="Sonny" from="Cher" />
+        <Hello to="Ringo" from="Paul" bangs={4} img="https://images.unsplash.com/photo-1558879177-69cde6b279c7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"/>
+        
       </div>
     );
   }
