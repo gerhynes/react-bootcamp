7210d786d753dcba22180cacd3b89eeb334c8c8e
Gerard Hynes <gkhynes@gmail.com>
2019-06-04 22:29:14 +0100

    Start building Pokemon project

---
+++ pokemon-project/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Pokedex from "./Pokedex";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Pokedex />
+    </div>
+  );
+}
+
+export default App;
---
+++ pokemon-project/src/Pokecard.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./Pokecard.css";
+const POKE_API =
+  " https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+export default class Pokecard extends Component {
+  render() {
+    let imgSrc = `${POKE_API}${this.props.id}.png`;
+    return (
+      <div className="Pokecard">
+        <h1 className="Pokecard-title">{this.props.name}</h1>
+        <img src={imgSrc} alt={this.props.name} />
+        <div className="Pokecard-data">Type: {this.props.type}</div>
+        <div className="Pokecard-data">Exp: {this.props.exp}</div>
+      </div>
+    );
+  }
+}
