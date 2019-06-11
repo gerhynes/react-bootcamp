cef14928a272feb3a77f45641a19a248055fb81b
Gerard Hynes <gkhynes@gmail.com>
2019-06-11 22:38:29 +0100

    Fix animation bug

--- dice-project/src/RollDice.js
+++ dice-project/src/RollDice.js
@@ -11,7 +11,7 @@ export default class RollDice extends Component {
     this.state = {
       die1: "one",
       die2: "one",
-      rolling: false
+      isRolling: false
     };
     this.roll = this.roll.bind(this);
   }
@@ -24,21 +24,21 @@ export default class RollDice extends Component {
       Math.floor(Math.random() * this.props.sides.length)
     ];
     // Set state with new numbers
-    this.setState({ die1: newDie1, die2: newDie2, rolling: true });
+    this.setState({ die1: newDie1, die2: newDie2, isRolling: true });
     // After one second, set isRolling to false
     setTimeout(() => {
-      this.setState({ rolling: false });
+      this.setState({ isRolling: false });
     }, 1000);
   }
   render() {
     return (
       <div className="RollDice">
         <div className="RollDice-container">
-          <Die face={this.state.die1} rolling={this.state.rolling} />
-          <Die face={this.state.die2} rolling={this.state.rolling} />
+          <Die face={this.state.die1} rolling={this.state.isRolling} />
+          <Die face={this.state.die2} rolling={this.state.isRolling} />
         </div>
-        <button onClick={this.roll} disabled={this.state.rolling}>
-          {this.state.rolling ? "Rolling..." : "Roll Dice!"}
+        <button onClick={this.roll} disabled={this.state.isRolling}>
+          {this.state.isRolling ? "Rolling..." : "Roll Dice!"}
         </button>
       </div>
     );
