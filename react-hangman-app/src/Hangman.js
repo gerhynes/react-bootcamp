139b307ab5e6008882b7e25022a0285069cbb39b
Gerard Hynes <gkhynes@gmail.com>
2019-06-22 16:59:05 +0100

    Set up files for hangman app

---
+++ react-hangman-app/src/Hangman.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import "./Hangman.css";
+import img0 from "./img/0.png";
+import img1 from "./img/1.png";
+import img2 from "./img/2.png";
+import img3 from "./img/3.png";
+import img4 from "./img/4.png";
+import img5 from "./img/5.png";
+import img6 from "./img/6.png";
+
+class Hangman extends Component {
+  /** by default, allow 6 guesses and use provided gallows images. */
+  static defaultProps = {
+    maxWrong: 6,
+    images: [img0, img1, img2, img3, img4, img5, img6]
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { nWrong: 0, guessed: new Set(), answer: "apple" };
+    this.handleGuess = this.handleGuess.bind(this);
+  }
+
+  /** guessedWord: show current-state of word:
+    if guessed letters are {a,p,e}, show "app_e" for "apple"
+  */
+  guessedWord() {
+    return this.state.answer
+      .split("")
+      .map(ltr => (this.state.guessed.has(ltr) ? ltr : "_"));
+  }
+
+  /** handleGuest: handle a guessed letter:
+    - add to guessed letters
+    - if not in answer, increase number-wrong guesses
+  */
+  handleGuess(evt) {
+    let ltr = evt.target.value;
+    this.setState(st => ({
+      guessed: st.guessed.add(ltr),
+      nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1)
+    }));
+  }
+
+  /** generateButtons: return array of letter buttons to render */
+  generateButtons() {
+    return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
+      <button
+        value={ltr}
+        onClick={this.handleGuess}
+        disabled={this.state.guessed.has(ltr)}
+      >
+        {ltr}
+      </button>
+    ));
+  }
+
+  /** render: render game */
+  render() {
+    return (
+      <div className="Hangman">
+        <h1>Hangman</h1>
+        <img src={this.props.images[this.state.nWrong]} />
+        <p className="Hangman-word">{this.guessedWord()}</p>
+        <p className="Hangman-btns">{this.generateButtons()}</p>
+      </div>
+    );
+  }
+}
+
+export default Hangman;
