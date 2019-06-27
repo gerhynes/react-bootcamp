07974ebd8d373a97dec092b00958e1e70c8a8822
Gerard Hynes <gkhynes@gmail.com>
2019-06-27 18:06:56 +0100

    Run build of lights out app

---
+++ lights-out/build/precache-manifest.c9ac656ae7024a065ad80553e7996620.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a85940fc4557d337f2f6",
+    "url": "/static/css/main.fa8c5604.chunk.css"
+  },
+  {
+    "revision": "a85940fc4557d337f2f6",
+    "url": "/static/js/main.a85940fc.chunk.js"
+  },
+  {
+    "revision": "207e5d056e3105dcde38",
+    "url": "/static/js/1.207e5d05.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "62686c074051f364922a3621979c2e5d",
+    "url": "/index.html"
+  }
+];
