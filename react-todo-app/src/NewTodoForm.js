9a5732231a33da39c7463a85d3277098ffb44303
Gerard Hynes <gkhynes@gmail.com>
2019-06-30 11:52:45 +0100

    Add ids to todos

---
+++ react-todo-app/src/NewTodoForm.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import uuid from "uuid/v4";
+
+export default class NewTodoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      task: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  }
+  handleSubmit(evt) {
+    evt.preventDefault();
+    this.props.createTodo({ ...this.state, id: uuid() });
+    this.setState({ task: "" });
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="task">New Todo</label>
+        <input
+          type="text"
+          placeholder="New Todo"
+          id="task"
+          name="task"
+          value={this.state.task}
+          onChange={this.handleChange}
+        />
+        <button>Add Todo</button>
+      </form>
+    );
+  }
+}
--- react-todo-app/src/TodoList.js
+++ react-todo-app/src/TodoList.js
@@ -1,20 +1,28 @@
 import React, { Component } from "react";
+import NewTodoForm from "./NewTodoForm";
 import Todo from "./Todo";
 
 export default class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: [{ task: "Walk the dog" }, { task: "Feed the birds" }]
+      todos: []
     };
+    this.create = this.create.bind(this);
+  }
+  create(newTodo) {
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
   }
   render() {
     const todos = this.state.todos.map(todo => {
-      return <Todo task={todo.task} />;
+      return <Todo key={todo.id} task={todo.task} />;
     });
     return (
       <div>
         <h1>Todo List</h1>
+        <NewTodoForm createTodo={this.create} />
         <ul>{todos}</ul>
       </div>
     );
