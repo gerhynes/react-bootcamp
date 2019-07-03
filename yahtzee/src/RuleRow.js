f085344802aec0eb701095f340295e3e899a594f
Gerard Hynes <gkhynes@gmail.com>
2019-07-03 22:21:57 +0100

    Prevent scores being reused

--- yahtzee/src/RuleRow.js
+++ yahtzee/src/RuleRow.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
-import './RuleRow.css'
+import React, { Component } from "react";
+import "./RuleRow.css";
 
 class RuleRow extends Component {
   render() {
+    const { score, name, doScore } = this.props;
+    const disabled = score !== undefined;
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
-        <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
+      <tr
+        className={`RuleRow RuleRow-${disabled ? "disabled" : "active"}`}
+        onClick={disabled ? null : doScore}
+      >
+        <td className="RuleRow-name">{name}</td>
+        <td className="RuleRow-score">{score}</td>
       </tr>
-    )
+    );
   }
 }
 
