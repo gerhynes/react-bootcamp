8ad4a03e3bd903919ae4c7ad9d08f500c5aab5a0
Gerard Hynes <gkhynes@gmail.com>
2019-07-03 22:09:10 +0100

    Fix dice rerolling bug

--- yahtzee/src/Game.js
+++ yahtzee/src/Game.js
@@ -47,13 +47,15 @@ class Game extends Component {
 
   toggleLocked(idx) {
     // toggle whether idx is in locked or not
-    this.setState(st => ({
-      locked: [
-        ...st.locked.slice(0, idx),
-        !st.locked[idx],
-        ...st.locked.slice(idx + 1)
-      ]
-    }));
+    if (this.state.rollsLeft > 0) {
+      this.setState(st => ({
+        locked: [
+          ...st.locked.slice(0, idx),
+          !st.locked[idx],
+          ...st.locked.slice(idx + 1)
+        ]
+      }));
+    }
   }
 
   doScore(rulename, ruleFn) {
@@ -81,7 +83,9 @@ class Game extends Component {
             <div className="Game-button-wrapper">
               <button
                 className="Game-reroll"
-                disabled={this.state.locked.every(x => x)}
+                disabled={
+                  this.state.locked.every(x => x) || this.state.rollsLeft === 0
+                }
                 onClick={this.roll}
               >
                 {this.state.rollsLeft} Rerolls Left
