e8a189a75bdb279fb7c8d516eb7e5fa86ddde422
Gerard Hynes <gkhynes@gmail.com>
2019-07-07 22:02:47 +0100

    Practice loading data with axios

--- lifecycle-methods/src/ZenQuote.js
+++ lifecycle-methods/src/ZenQuote.js
@@ -1,21 +1,30 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "./ZenQuote.css";
 
 export default class ZenQuote extends Component {
   constructor(props) {
     super(props);
-    this.state = { quote: "" };
+    this.state = { quote: "", isLoaded: false };
   }
   componentDidMount() {
     axios.get("https://api.github.com/zen").then(response => {
-      this.setState({ quote: response.data });
+      setTimeout(() => {
+        this.setState({ quote: response.data, isLoaded: true });
+      }, 3000);
     });
   }
   render() {
     return (
       <div>
-        <h1>Always remember...</h1>
-        <p>{this.state.quote}</p>
+        {this.state.isLoaded ? (
+          <div>
+            <h1>Always remember...</h1>
+            <p>{this.state.quote}</p>
+          </div>
+        ) : (
+          <div className="loader" />
+        )}
       </div>
     );
   }
