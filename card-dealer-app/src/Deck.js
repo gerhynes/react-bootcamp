e2e1ff45163829746b7ee3e31f462af5b2d36e03
Gerard Hynes <gkhynes@gmail.com>
2019-07-09 19:16:56 +0100

    Style rest of app

--- card-dealer-app/src/Deck.js
+++ card-dealer-app/src/Deck.js
@@ -50,8 +50,13 @@ export default class Deck extends Component {
     });
     return (
       <div className="Deck">
-        <h1>Card Dealer</h1>
-        <button onClick={this.getCard}>Get Card!</button>
+        <h1 className="Deck-title">♦ Card Dealer ♦</h1>
+        <h2 className="Deck-title subtitle">
+          ♦ A little demo made with React ♦
+        </h2>
+        <button className="Deck-btn" onClick={this.getCard}>
+          Get Card!
+        </button>
         <div className="Deck-cardarea">{cards}</div>
       </div>
     );
