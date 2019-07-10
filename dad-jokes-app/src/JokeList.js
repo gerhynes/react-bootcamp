f03f8a8cb50a899d5c050e3b69a7e2edf5913473
Gerard Hynes <gkhynes@gmail.com>
2019-07-10 21:31:07 +0100

    Add dad-jokes-app starter files and nothing else because CRA is no good today

---
+++ dad-jokes-app/src/JokeList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class JokeList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Dad Jokes</h1>
+      </div>
+    )
+  }
+}
