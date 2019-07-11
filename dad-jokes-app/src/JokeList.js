a973780b031c83f80a1e786881432dc1a2b02955
Gerard Hynes <gkhynes@gmail.com>
2019-07-11 12:25:19 +0100

    Fetch and render jokes

--- dad-jokes-app/src/JokeList.js
+++ dad-jokes-app/src/JokeList.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class JokeList extends Component {
+  static defaultProps = {
+    numJokesToGet: 10
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      jokes: []
+    };
+  }
+  async componentDidMount() {
+    let jokes = [];
+    while (jokes.length < this.props.numJokesToGet) {
+      let res = await axios.get("https://icanhazdadjoke.com/", {
+        headers: { Accept: "application/json" }
+      });
+      jokes.push(res.data.joke);
+    }
+    this.setState({ jokes: jokes });
+  }
   render() {
     return (
-      <div>
+      <div className="JokeList">
         <h1>Dad Jokes</h1>
+        <div className="JokeList-jokes">
+          {this.state.jokes.map(j => (
+            <div>{j}</div>
+          ))}
+        </div>
       </div>
-    )
+    );
   }
 }
