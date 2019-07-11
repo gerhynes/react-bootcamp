9c2e4c9a3c338d9393c2339e2f19fdd188f4e9c5
Gerard Hynes <gkhynes@gmail.com>
2019-07-11 12:43:40 +0100

    Add main styles to app

--- dad-jokes-app/src/JokeList.js
+++ dad-jokes-app/src/JokeList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "./JokeList.css";
 
 export default class JokeList extends Component {
   static defaultProps = {
@@ -24,7 +25,16 @@ export default class JokeList extends Component {
   render() {
     return (
       <div className="JokeList">
-        <h1>Dad Jokes</h1>
+        <div className="JokeList-sidebar">
+          <h1 className="JokeList-title">
+            <span>Dad</span> Jokes
+          </h1>
+          <img
+            src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+            alt="Crying Laughing Emoji"
+          />
+          <button className="JokeList-getmore">New Jokes</button>
+        </div>
         <div className="JokeList-jokes">
           {this.state.jokes.map(j => (
             <div>{j}</div>
