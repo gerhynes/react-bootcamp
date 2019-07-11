d098c34a42301769958b3b2f1b568decff5e96d8
Gerard Hynes <gkhynes@gmail.com>
2019-07-11 16:28:38 +0100

    Style Joke component

--- dad-jokes-app/src/Joke.js
+++ dad-jokes-app/src/Joke.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./Joke.css";
 
 export default class Joke extends Component {
   render() {
@@ -6,10 +7,13 @@ export default class Joke extends Component {
       <div className="Joke">
         <div className="Joke-buttons">
           <i className="fas fa-arrow-up" onClick={this.props.upvote} />
-          <span>{this.props.votes}</span>
+          <span className="Joke-votes">{this.props.votes}</span>
           <i className="fas fa-arrow-down" onClick={this.props.downvote} />
         </div>
         <div className="Joke-text">{this.props.text}</div>
+        <div className="Joke-smiley">
+          <i className="em em-rolling_on_the_floor_laughing" />
+        </div>
       </div>
     );
   }
