d5527c1b010367eedf9068c14ea0ff0d4c4c10fe
Gerard Hynes <gkhynes@gmail.com>
2019-07-15 17:58:16 +0100

    Save jokes to localStorage

--- dad-jokes-app/src/JokeList.js
+++ dad-jokes-app/src/JokeList.js
@@ -11,10 +11,14 @@ export default class JokeList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      jokes: []
+      jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]")
     };
   }
-  async componentDidMount() {
+  componentDidMount() {
+    if (this.state.jokes.length === 0) this.getJokes();
+  }
+
+  async getJokes() {
     let jokes = [];
     while (jokes.length < this.props.numJokesToGet) {
       let res = await axios.get("https://icanhazdadjoke.com/", {
@@ -23,6 +27,7 @@ export default class JokeList extends Component {
       jokes.push({ id: uuid(), text: res.data.joke, votes: 0 });
     }
     this.setState({ jokes: jokes });
+    window.localStorage.setItem("jokes", JSON.stringify(jokes));
   }
   handleVote(id, delta) {
     this.setState(st => ({
