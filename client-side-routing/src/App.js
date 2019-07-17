8a70faedccb9446768d50e942ca0503a0494f16b
Gerard Hynes <gkhynes@gmail.com>
2019-07-17 21:10:17 +0100

    Add Switch and exact

--- client-side-routing/src/App.js
+++ client-side-routing/src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import About from "./About";
 import Dog from "./Dog";
 import Contact from "./Contact";
 import "./App.css";
 
+const Hater = () => <h1>I ABSOLUTELY HATE DOGS!</h1>;
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/" component={About} />
-        <Route path="/dog" component={Dog} />
-        <Route path="/contact" component={Contact} />
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/dog" component={Dog} />
+          <Route exact path="/dog/hater" component={Hater} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
       </div>
     );
   }
