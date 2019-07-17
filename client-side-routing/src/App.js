16fad42e040668c37f497a2e4abb2482d632e4bf
Gerard Hynes <gkhynes@gmail.com>
2019-07-17 21:27:02 +0100

    Practice working with Link and NavLink components

--- client-side-routing/src/App.js
+++ client-side-routing/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, NavLink } from "react-router-dom";
 import About from "./About";
 import Dog from "./Dog";
 import Contact from "./Contact";
@@ -11,6 +11,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <nav className="App-nav">
+          <NavLink exact activeClassName="active-link" to="/">
+            About
+          </NavLink>
+          <NavLink exact activeClassName="active-link" to="/dog">
+            Dog
+          </NavLink>
+          <NavLink exact activeClassName="active-link" to="/contact">
+            Contact
+          </NavLink>
+        </nav>
         <Switch>
           <Route exact path="/" component={About} />
           <Route exact path="/dog" component={Dog} />
