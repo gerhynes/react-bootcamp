8c21dfd3a04f823e530c16734709ecd1df409bb2
Gerard Hynes <gkhynes@gmail.com>
2019-07-21 17:16:01 +0100

    Practice working with URL params

---
+++ react-router-patterns/src/Food.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./Food.css";
+
+class Food extends Component {
+  render() {
+    const name = this.props.match.params.name;
+    const url = `https://source.unsplash.com/1600x900/?${name}`;
+    return (
+      <div className="Food">
+        <h1>I love to eat {name}</h1>
+        <img src={url} alt={name} />
+      </div>
+    );
+  }
+}
+
+export default Food;
