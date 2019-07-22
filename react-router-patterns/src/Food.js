6c994ceefff4772cf45807c94c944c2a92178c8b
Gerard Hynes <gkhynes@gmail.com>
2019-07-22 21:08:49 +0100

    Add Redirect component

--- react-router-patterns/src/Food.js
+++ react-router-patterns/src/Food.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 import "./Food.css";
 
 class Food extends Component {
@@ -7,8 +8,14 @@ class Food extends Component {
     const url = `https://source.unsplash.com/1600x900/?${name}`;
     return (
       <div className="Food">
-        <h1>I love to eat {name}</h1>
-        <img src={url} alt={name} />
+        {/\d/.test(name) ? (
+          <Redirect to="/" />
+        ) : (
+          <div>
+            <h1>I love to eat {name}</h1>
+            <img src={url} alt={name} />
+          </div>
+        )}
       </div>
     );
   }
