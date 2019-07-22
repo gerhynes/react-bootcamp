e48c0bff5289b8c380f888594d6f2f75469948fc
Gerard Hynes <gkhynes@gmail.com>
2019-07-22 21:30:15 +0100

    Practice the difference between Redirect and history.push()

--- react-router-patterns/src/Food.js
+++ react-router-patterns/src/Food.js
@@ -5,11 +5,12 @@ import "./Food.css";
 class Food extends Component {
   render() {
     const name = this.props.match.params.name;
+    if (/\d/.test(name)) this.props.history.push("/notfound");
     const url = `https://source.unsplash.com/1600x900/?${name}`;
     return (
       <div className="Food">
         {/\d/.test(name) ? (
-          <Redirect to="/" />
+          <Redirect to="/notfound" />
         ) : (
           <div>
             <h1>I love to eat {name}</h1>
