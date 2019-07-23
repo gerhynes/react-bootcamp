1605ca609472d0d14e7bfcf11866eff071f9368d
Gerard Hynes <gkhynes@gmail.com>
2019-07-23 22:09:51 +0100

    Add DogDetails component

--- dog-finder-app/src/App.js
+++ dog-finder-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import DogList from "./DogList";
+import DogDetails from "./DogDetails";
 import { Route, Switch } from "react-router-dom";
 import whiskey from "./imgs/whiskey.jpg";
 import tubby from "./imgs/tubby.jpg";
@@ -42,8 +43,22 @@ class App extends Component {
     ]
   };
   render() {
+    const getDog = props => {
+      let name = props.match.params.name;
+      let currentDog = this.props.dogs.find(
+        dog => dog.name.toLowerCase() === name.toLowerCase()
+      );
+      return <DogDetails {...props} dog={currentDog} />;
+    };
     return (
-      <Route path="/dogs" render={() => <DogList dogs={this.props.dogs} />} />
+      <Switch>
+        <Route
+          exact
+          path="/dogs"
+          render={() => <DogList dogs={this.props.dogs} />}
+        />
+        <Route exact path="/dogs/:name" render={getDog} />
+      </Switch>
     );
   }
 }
