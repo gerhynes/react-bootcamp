c7227a32f11fe20d0a79c1e09cce5c5629cb581c
Gerard Hynes <gkhynes@gmail.com>
2019-07-23 21:32:26 +0100

    Set up files for dog-finder app

---
+++ dog-finder-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="display-1">Dog Finder</h1>
+    </div>
+  );
+}
+
+export default App;
