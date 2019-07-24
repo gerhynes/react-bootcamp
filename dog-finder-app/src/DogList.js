277c36269570c47150ad65c3062ca06a57d1b996
Gerard Hynes <gkhynes@gmail.com>
2019-07-24 22:59:59 +0100

    Add custom styles

--- dog-finder-app/src/DogList.js
+++ dog-finder-app/src/DogList.js
@@ -6,16 +6,15 @@ export default class extends Component {
   render() {
     return (
       <div className="DogList">
-        <h1 className="display-1 text-center">Dog List</h1>
-
+        <h1 className="display-1 text-center my-4">Give a dog a good home</h1>
         <div className="row">
           {this.props.dogs.map(d => {
             return (
               <div className="col-md-6 col-lg-4 text-center Dog" key={d.name}>
-                <img src={d.src} alt={d.name} />
-                <h3>
-                  <Link to={`/dogs/${d.name}`}>{d.name}</Link>
-                </h3>
+                <Link to={`/dogs/${d.name}`}>
+                  <img src={d.src} alt={d.name} />
+                  <h3 className="underline mt-3">{d.name}</h3>
+                </Link>
               </div>
             );
           })}
