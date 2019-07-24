75edee75025efd03e34525c08f443dfd30b8d0de
Gerard Hynes <gkhynes@gmail.com>
2019-07-24 22:32:04 +0100

    Clean up Bootstrap markup

--- dog-finder-app/src/DogDetails.js
+++ dog-finder-app/src/DogDetails.js
@@ -6,29 +6,25 @@ export default class DogDetails extends Component {
   render() {
     let { dog } = this.props;
     return (
-      <div className="container">
-        <div className="DogDetails row justify-content-center mt-5">
-          <div className="col-11 col-lg-5">
-            <div className="DogDetails-card card">
-              <img className="card-img-top" src={dog.src} alt={dog.name} />
-              <div className="card-body">
-                <h2 className="card-title">{dog.name}</h2>
-                <h4 className="card-subtitle text-muted">
-                  {dog.age} years old
-                </h4>
-              </div>
-              <ul className="list-group list-group-flush">
-                {dog.facts.map((fact, i) => (
-                  <li className="list-group-item" key={i}>
-                    {fact}
-                  </li>
-                ))}
-              </ul>
-              <div className="card-body">
-                <Link className="btn  btn-info" to="/dogs">
-                  Go Back
-                </Link>
-              </div>
+      <div className="DogDetails row justify-content-center mt-5">
+        <div className="col-11 col-lg-5">
+          <div className="DogDetails-card card">
+            <img className="card-img-top" src={dog.src} alt={dog.name} />
+            <div className="card-body">
+              <h2 className="card-title">{dog.name}</h2>
+              <h4 className="card-subtitle text-muted">{dog.age} years old</h4>
+            </div>
+            <ul className="list-group list-group-flush">
+              {dog.facts.map((fact, i) => (
+                <li className="list-group-item" key={i}>
+                  {fact}
+                </li>
+              ))}
+            </ul>
+            <div className="card-body">
+              <Link className="btn  btn-info" to="/dogs">
+                Go Back
+              </Link>
             </div>
           </div>
         </div>
--- dog-finder-app/src/DogList.js
+++ dog-finder-app/src/DogList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./DogList.css";
 
 export default class extends Component {
@@ -6,17 +7,18 @@ export default class extends Component {
     return (
       <div className="DogList">
         <h1 className="display-1 text-center">Dog List</h1>
-        <div className="container">
-          <div className="row">
-            {this.props.dogs.map(d => {
-              return (
-                <div className="col-md-6 col-lg-4 text-center Dog" key={d.name}>
-                  <img src={d.src} alt={d.name} />
-                  <h3>{d.name}</h3>
-                </div>
-              );
-            })}
-          </div>
+
+        <div className="row">
+          {this.props.dogs.map(d => {
+            return (
+              <div className="col-md-6 col-lg-4 text-center Dog" key={d.name}>
+                <img src={d.src} alt={d.name} />
+                <h3>
+                  <Link to={`/dogs/${d.name}`}>{d.name}</Link>
+                </h3>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
