b01c3c08b6328dadbf1804e4de607915385dc698
Gerard Hynes <gkhynes@gmail.com>
2019-08-30 18:23:27 +0100

    Set up initial files

---
+++ context-app/src/Navbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/core/Search";
+import Switch from "@material-ui/core/Switch";
+import { withStyles } from "@material-ui/core/styles";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Navbar!</h1>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
