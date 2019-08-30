c415157066a48fd5cc4ba14466ca2c794d260e6a
Gerard Hynes <gkhynes@gmail.com>
2019-08-30 21:03:19 +0100

    Add responsive Navbar

--- context-app/src/Navbar.js
+++ context-app/src/Navbar.js
@@ -4,18 +4,42 @@ import ToolBar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import InputBase from "@material-ui/core/InputBase";
-import SearchIcon from "@material-ui/core/Search";
+import SearchIcon from "@material-ui/icons/Search";
 import Switch from "@material-ui/core/Switch";
 import { withStyles } from "@material-ui/core/styles";
+import styles from "./styles/NavbarStyles";
 
 class Navbar extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <h1>Navbar!</h1>
+      <div className={classes.root}>
+        <AppBar position="static" color="primary">
+          <ToolBar>
+            <IconButton className={classes.menuButton} color="inherit">
+              <span role="img" aria-label="shades-emoji">
+                😎
+              </span>
+            </IconButton>
+            <Typography className={classes.title} variant="h6" color="inherit">
+              App Title
+            </Typography>
+            <Switch />
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placehiolder="Search..."
+                classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              />
+            </div>
+          </ToolBar>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
