06e4d6b5babad1c01a1b581635e3a63b6447b9cf
Gerard Hynes <gkhynes@gmail.com>
2019-09-02 21:59:58 +0100

    Refactor Form to be a functional component

--- context-app/src/Form.js
+++ context-app/src/Form.js
@@ -1,7 +1,6 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
-import CssBaseLine from "@material-ui/core/CssBaseline";
 import FormControl from "@material-ui/core/FormControl";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
@@ -55,54 +54,51 @@ const words = {
   }
 };
 
-class Form extends Component {
-  static contextType = LanguageContext;
-  render() {
-    const { classes } = this.props;
-    const { language, changeLanguage } = this.context;
-    const { email, signIn, password, rememberMe } = words[language];
-    return (
-      <main className={classes.main}>
-        <Paper className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedicon></LockOutlinedicon>
-          </Avatar>
-          <Typography variant="h5">{signIn}</Typography>
-          <Select value={language} onChange={changeLanguage}>
-            <MenuItem value="english">English</MenuItem>
-            <MenuItem value="gaeilge">Gaeilge</MenuItem>
-            <MenuItem value="français">Français</MenuItem>
-            <MenuItem value="deutsch">Deutsch</MenuItem>
-            <MenuItem value="español">Español</MenuItem>
-            <MenuItem value="italiano">Italiano</MenuItem>
-          </Select>
-          <form className={classes.form}>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="email">{email}</InputLabel>
-              <Input id="email" name="email" autoFocus></Input>
-            </FormControl>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="password">{password}</InputLabel>
-              <Input id="password" name="password" autoFocus></Input>
-            </FormControl>
-            <FormControlLabel
-              control={<Checkbox color="primary" />}
-              label={rememberMe}
-            />
-            <Button
-              variant="contained"
-              type="submit"
-              fullWidth
-              color="primary"
-              className={classes.submit}
-            >
-              {signIn}
-            </Button>
-          </form>
-        </Paper>
-      </main>
-    );
-  }
+function Form(props) {
+  const { language, changeLanguage } = useContext(LanguageContext);
+  const { classes } = props;
+  const { email, signIn, password, rememberMe } = words[language];
+  return (
+    <main className={classes.main}>
+      <Paper className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedicon></LockOutlinedicon>
+        </Avatar>
+        <Typography variant="h5">{signIn}</Typography>
+        <Select value={language} onChange={changeLanguage}>
+          <MenuItem value="english">English</MenuItem>
+          <MenuItem value="gaeilge">Gaeilge</MenuItem>
+          <MenuItem value="français">Français</MenuItem>
+          <MenuItem value="deutsch">Deutsch</MenuItem>
+          <MenuItem value="español">Español</MenuItem>
+          <MenuItem value="italiano">Italiano</MenuItem>
+        </Select>
+        <form className={classes.form}>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="email">{email}</InputLabel>
+            <Input id="email" name="email" autoFocus></Input>
+          </FormControl>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="password">{password}</InputLabel>
+            <Input id="password" name="password" autoFocus></Input>
+          </FormControl>
+          <FormControlLabel
+            control={<Checkbox color="primary" />}
+            label={rememberMe}
+          />
+          <Button
+            variant="contained"
+            type="submit"
+            fullWidth
+            color="primary"
+            className={classes.submit}
+          >
+            {signIn}
+          </Button>
+        </form>
+      </Paper>
+    </main>
+  );
 }
 
 export default withStyles(styles)(Form);
