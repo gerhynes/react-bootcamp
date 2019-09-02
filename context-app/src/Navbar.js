a8cc34494f1a5feb95155fc8cdb3127bba652640
Gerard Hynes <gkhynes@gmail.com>
2019-09-02 22:05:46 +0100

    Refactor Navbar to be functional component

--- context-app/src/Navbar.js
+++ context-app/src/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import ToolBar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
@@ -9,7 +9,7 @@ import Switch from "@material-ui/core/Switch";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles/NavbarStyles";
 import { ThemeContext } from "./contexts/ThemeContext";
-import { withLanguageContext } from "./contexts/LanguageContext";
+import { LanguageContext } from "./contexts/LanguageContext";
 
 const content = {
   english: {
@@ -38,41 +38,38 @@ const content = {
   }
 };
 
-class Navbar extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isDarkMode, toggleTheme } = this.context;
-    const { classes } = this.props;
-    const { language } = this.props.languageContext;
-    const { search, emoji } = content[language];
-    return (
-      <div className={classes.root}>
-        <AppBar position="static" color={isDarkMode ? "default" : "primary"}>
-          <ToolBar>
-            <IconButton className={classes.menuButton} color="inherit">
-              <span role="img" aria-label="shades-emoji">
-                {emoji}
-              </span>
-            </IconButton>
-            <Typography className={classes.title} variant="h6" color="inherit">
-              Hooks Login
-            </Typography>
-            <Switch onChange={toggleTheme} />
-            <div className={classes.grow} />
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder={`${search}...`}
-                classes={{ root: classes.inputRoot, input: classes.inputInput }}
-              />
+function Navbar(props) {
+  const { isDarkMode, toggleTheme } = useContext(ThemeContext);
+  const { language } = useContext(LanguageContext);
+  const { classes } = props;
+  const { search, emoji } = content[language];
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color={isDarkMode ? "default" : "primary"}>
+        <ToolBar>
+          <IconButton className={classes.menuButton} color="inherit">
+            <span role="img" aria-label="shades-emoji">
+              {emoji}
+            </span>
+          </IconButton>
+          <Typography className={classes.title} variant="h6" color="inherit">
+            Hooks Login
+          </Typography>
+          <Switch onChange={toggleTheme} />
+          <div className={classes.grow} />
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
             </div>
-          </ToolBar>
-        </AppBar>
-      </div>
-    );
-  }
+            <InputBase
+              placeholder={`${search}...`}
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+            />
+          </div>
+        </ToolBar>
+      </AppBar>
+    </div>
+  );
 }
 
-export default withLanguageContext(withStyles(styles)(Navbar));
+export default withStyles(styles)(Navbar);
--- context-app/src/contexts/LanguageContext.js
+++ context-app/src/contexts/LanguageContext.js
@@ -26,8 +26,8 @@ export class LanguageProvider extends Component {
   }
 }
 
-export const withLanguageContext = Component => props => (
-  <LanguageContext.Consumer>
-    {value => <Component languageContext={value} {...props} />}
-  </LanguageContext.Consumer>
-);
+// export const withLanguageContext = Component => props => (
+//   <LanguageContext.Consumer>
+//     {value => <Component languageContext={value} {...props} />}
+//   </LanguageContext.Consumer>
+// );
