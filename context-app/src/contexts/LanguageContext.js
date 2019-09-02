49ea22b80efd585747f91f1e87def3433946a22f
Gerard Hynes <gkhynes@gmail.com>
2019-09-02 22:12:09 +0100

    Refactor LanguageContext as a hooks-based component

--- context-app/src/contexts/LanguageContext.js
+++ context-app/src/contexts/LanguageContext.js
@@ -1,33 +1,13 @@
-import React, { createContext, Component } from "react";
+import React, { createContext, useState } from "react";
 
 export const LanguageContext = createContext();
 
-export class LanguageProvider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      language: "gaeilge"
-    };
-    this.changeLanguage = this.changeLanguage.bind(this);
-  }
-  changeLanguage(e) {
-    this.setState({
-      language: e.target.value
-    });
-  }
-  render() {
-    return (
-      <LanguageContext.Provider
-        value={{ ...this.state, changeLanguage: this.changeLanguage }}
-      >
-        {this.props.children}
-      </LanguageContext.Provider>
-    );
-  }
+export function LanguageProvider(props) {
+  const [language, setLanguage] = useState("gaeilge");
+  const changeLanguage = e => setLanguage(e.target.value);
+  return (
+    <LanguageContext.Provider value={{ language, changeLanguage }}>
+      {props.children}
+    </LanguageContext.Provider>
+  );
 }
-
-// export const withLanguageContext = Component => props => (
-//   <LanguageContext.Consumer>
-//     {value => <Component languageContext={value} {...props} />}
-//   </LanguageContext.Consumer>
-// );
