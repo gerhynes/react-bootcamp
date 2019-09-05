af16763527374791e7fc4d50291cacda0969fb0c
Gerard Hynes <gkhynes@gmail.com>
2019-09-05 23:23:14 +0100

    Define Todo Reducer

--- hooks-todo-app/src/contexts/todos.context.js
+++ hooks-todo-app/src/contexts/todos.context.js
@@ -1,4 +1,5 @@
-import React, { createContext } from "react";
+import React, { createContext, useReducer } from "react";
+import todoReducer from "../reducers/todo.reducer";
 import useTodoState from "../hooks/useTodoState";
 
 const defaultTodos = [
@@ -9,9 +10,9 @@ const defaultTodos = [
 export const TodosContext = createContext();
 
 export function TodosProvider(props) {
-  const todosStuff = useTodoState(defaultTodos);
+  const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
   return (
-    <TodosContext.Provider value={todosStuff}>
+    <TodosContext.Provider value={{ todos, dispatch }}>
       {props.children}
     </TodosContext.Provider>
   );
