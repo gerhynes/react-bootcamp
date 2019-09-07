9aced41a6cb38578dc9b82ba742899cf70672cc3
Gerard Hynes <gkhynes@gmail.com>
2019-09-07 17:48:37 +0100

    Split Context into two

--- hooks-todo-app/src/TodoForm.js
+++ hooks-todo-app/src/TodoForm.js
@@ -2,11 +2,11 @@ import React, { useContext } from "react";
 import useInputState from "./hooks/useInputState";
 import Paper from "@material-ui/core/Paper";
 import TextField from "@material-ui/core/TextField";
-import { TodosContext } from "./contexts/todos.context";
+import { DispatchContext } from "./contexts/todos.context";
 
 function TodoForm() {
   const [value, handleChange, reset] = useInputState("");
-  const { dispatch } = useContext(TodosContext);
+  const dispatch = useContext(DispatchContext);
   return (
     <Paper style={{ margin: "1rem 0", padding: "0 1rem" }}>
       <form
--- hooks-todo-app/src/contexts/todos.context.js
+++ hooks-todo-app/src/contexts/todos.context.js
@@ -8,12 +8,15 @@ const defaultTodos = [
 ];
 
 export const TodosContext = createContext();
+export const DispatchContext = createContext();
 
 export function TodosProvider(props) {
   const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
   return (
-    <TodosContext.Provider value={{ todos, dispatch }}>
-      {props.children}
+    <TodosContext.Provider value={todos}>
+      <DispatchContext.Provider value={dispatch}>
+        {props.children}
+      </DispatchContext.Provider>
     </TodosContext.Provider>
   );
 }
