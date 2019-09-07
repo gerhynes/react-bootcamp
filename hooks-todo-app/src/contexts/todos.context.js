20676a51330128cb49c64971d227ba04415dea6b
Gerard Hynes <gkhynes@gmail.com>
2019-09-07 18:07:49 +0100

    Create custom hook for syncing localStorage

--- hooks-todo-app/src/contexts/todos.context.js
+++ hooks-todo-app/src/contexts/todos.context.js
@@ -1,4 +1,5 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext } from "react";
+import { useLocalStorageReducer } from "../hooks/useLocalStorageReducer";
 import todoReducer from "../reducers/todo.reducer";
 
 const defaultTodos = [
@@ -10,7 +11,11 @@ export const TodosContext = createContext();
 export const DispatchContext = createContext();
 
 export function TodosProvider(props) {
-  const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
+  const [todos, dispatch] = useLocalStorageReducer(
+    "todos",
+    defaultTodos,
+    todoReducer
+  );
   return (
     <TodosContext.Provider value={todos}>
       <DispatchContext.Provider value={dispatch}>
